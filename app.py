97a998abb95ad8b8ac87b916c766da2e9b92f8e0
Noelle Browne <noelle.browne@yahoo.ie>
2019-03-01 20:30:31 +0000

    added requirements.txt

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def hello():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)insta
+            debug=True)
